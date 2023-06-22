635744351f918e8c39cd1462520ffff2354387ac
ljimshitashvili <jimshitashvili4444@gmail.com>
2023-06-22 18:13:28 +0400

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <div className="flex w-full min-h-screen flex-col bg-[url('./assets/pattern-bg-mobile.png')] bg-no-repeat bg-x-center bg-contain">
+      <Header />
+    </div>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import "../index.css";
+
+const Header = () => {
+  return (
+    <h1 className="text-white text-shadow font-medium text-[32px] leading-[30px] w-full text-center pt-[33px] pb-[12px] tracking-[-0.286px]">
+      IP Address Tracker
+    </h1>
+  );
+};
+
+export default Header;
